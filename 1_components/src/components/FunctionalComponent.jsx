3862f7f9f952601b333c58a89b3228b409051fe3
jfranco <jefersonfranco@pm.me>
2025-01-29 18:05:58 -0300

    docs(components): update 1 file and create 1 file

---
+++ 1_components/src/components/FunctionalComponent.jsx
@@ -0,0 +1,14 @@
+// componente filho
+
+// 1:Cria funçao
+function FunctionalComponent() {
+  // 2:Retorna um objeto com jsx
+  return (
+    <div>
+      <h2>Testando componente</h2>
+    </div>
+  );
+}
+
+// 3:Exporta todos os dados
+export default FunctionalComponent;
