078605830f810f0538247171573bc958c4868be6
jfranco <jefersonfranco@pm.me>
2025-01-29 18:15:42 -0300

    docs(components): update 1 file and create 1 file

--- 1_components/src/App.jsx
+++ 1_components/src/App.jsx
@@ -3,13 +3,15 @@ import "./App.css";
 
 // 4:Importa o componente filho
 import FunctionalComponent from "./components/FunctionalComponent";
+import ClassComponent from "./components/ClassComponent";
 
 function App() {
   return (
     <>
       <h1>Hello World React</h1>
       {/* 5:Utiliza o componente importado no JSX */}
-      <FunctionalComponent/>
+      <FunctionalComponent />
+      <ClassComponent/>
     </>
   );
 }
