0043970b75852630c185ba7f0962c994003c16cb
jfranco <jefersonfranco@pm.me>
2025-01-29 18:50:29 -0300

    docs(jsx): update JsxExamples.jsx

--- 2_jsx/src/components/JsxExamples.jsx
+++ 2_jsx/src/components/JsxExamples.jsx
@@ -12,8 +12,11 @@ const JsxExamples = () => {
     return `Olá ${name}`;
   }
 
+  const isUserLoggedIn = false;
+
   return (
     <div>
+      {/* BASICO */}
       <h2>Conteúdo que o usuário vai ver</h2>
       {/* Listando dados do usuário */}
       <p>O nome do usuário é: {userName}</p>
@@ -26,6 +29,26 @@ const JsxExamples = () => {
 
       <p>{getGreeting(userName)}</p>
       <p>{getGreeting("Jeff")}</p>
+
+      {/* DIFERENCAS DO HTML */}
+      <div className="alguma-coisa">Este cara</div>
+
+      <div className="teste">ok</div>
+
+      <button onClick={() => alert("teste")}>clique em mim</button>
+
+      <input type="text" placeholder="Digite algo" />
+
+      {/* RENDERIZACAO CONDICIONAL */}
+      {isUserLoggedIn ? (
+        <div>
+          <p>Caso: está logado</p>
+        </div>
+      ) : (
+        <div>
+          <p>Caso: NAO está logado</p>
+        </div>
+      )}
     </div>
   );
 };
