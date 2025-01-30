5e0ab6d145cacb61053de916fd643ad7db3f838b
jfranco <jefersonfranco@pm.me>
2025-01-30 10:31:53 -0300

    docs(events): update EventHandlingExamples.jsx

--- 4_events/src/components/EventHandlingExamples.jsx
+++ 4_events/src/components/EventHandlingExamples.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 
 const EventHandlingExamples = () => {
+  const handleClick = () => {
+    alert("Testando");
+  };
+
   return (
     <div>
       <button onClick={() => alert("oi")}>Clique aqui</button>
+      <button onClick={handleClick}>Clique aqui</button>
     </div>
   );
 };
