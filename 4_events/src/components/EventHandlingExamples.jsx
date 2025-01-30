aa5213ce0610dd8dc70e25afe4f8699f2da37feb
jfranco <jefersonfranco@pm.me>
2025-01-30 15:30:58 -0300

    docs(events): update EventHandlingExamples.jsx

--- 4_events/src/components/EventHandlingExamples.jsx
+++ 4_events/src/components/EventHandlingExamples.jsx
@@ -5,10 +5,17 @@ const EventHandlingExamples = () => {
     alert("Testando");
   };
 
+  const handleGreet = (name) => {
+    alert(`Olá ${name}`);
+  };
+
   return (
     <div>
       <button onClick={() => alert("oi")}>Clique aqui</button>
       <button onClick={handleClick}>Clique aqui 2</button>
+      <br />
+      <button onClick={() => handleGreet("Leonardo")}>Dizer Olá Leonardo</button>
+      <button onClick={() => handleGreet("Ayrton")}>Dizer Olá Ayrton</button>
     </div>
   );
 };
