761559a78f8a424c8c77155c7b11f6afb58affd2
jfranco <jefersonfranco@pm.me>
2025-01-30 10:21:21 -0300

    docs(state-mgt): update UseStateComponent.jsx

--- 3_state_management/src/components/UseStateComponent.jsx
+++ 3_state_management/src/components/UseStateComponent.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 // useState = hooks
 
+// sempre que for alterar valor de algo => useState
+// se somente leitura => var, state
+
 const UseStateComponent = () => {
   // cria uma variavel de consulta, e uma variavel de alteraçao, e inicia o hook
   const [count, setCount] = useState(0);
@@ -12,11 +15,30 @@ const UseStateComponent = () => {
     console.log(count);
   };
 
+  const [user, setUser] = useState({
+    name: "Albert",
+    age: 76,
+    hobbies: ["sailing", "reading"],
+  });
+
+  const updateUserAge = () => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      age: prevUser.age + 1,
+    }));
+  };
+
   return (
     <div>
       <h2>Contador</h2>
       <p>Você clicou {count} vezes</p>
       <button onClick={increment}>Incrementar</button>
+
+      <p>
+        Nome: {user.name} e idade: {user.age}
+      </p>
+
+      <button onClick={updateUserAge}>Incrementar Idade</button>
     </div>
   );
 };
