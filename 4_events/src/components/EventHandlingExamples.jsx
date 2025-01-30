bc1e2f884c3123c1e51bbe7fbc79b5e255d34643
jfranco <jefersonfranco@pm.me>
2025-01-30 10:29:58 -0300

    docs(events): update 1 file and create 1 file

---
+++ 4_events/src/components/EventHandlingExamples.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const EventHandlingExamples = () => {
+  return (
+    <div>
+      <button onClick={() => alert("oi")}>Clique aqui</button>
+    </div>
+  );
+};
+
+export default EventHandlingExamples;
