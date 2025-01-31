4c1174eff259c4f37663811680d9c6b880863961
jfranco <jefersonfranco@pm.me>
2025-01-30 23:18:33 -0300

    docs(project): update TodoApp.css and TodoApp.jsx

--- 7_project/src/components/TodoApp.jsx
+++ 7_project/src/components/TodoApp.jsx
@@ -6,10 +6,29 @@ const TodoApp = () => {
   // lista de tarefas
   const [todos, setTodos] = useState([]);
 
+  // estado de texto da tarefa
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
+    <div className="app-container">
       <h1 className="title">Lista de Tarefas</h1>
       {/* form pra adicionar tarefas */}
+      <form obSubmit={handleSubmit} className="form-container">
+        <input
+          type="text"
+          className="input-field"
+          placeholder="Adicione uma tarefa..."
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="submit" className="add-button">
+          Adicionar
+        </button>
+      </form>
 
       {/* lista de tarefas */}
       {todos.length === 0 && <p className="empty">nao há tarefas.</p>}
